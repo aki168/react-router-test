bedef39cf29859c5852866f5ca47fcf496d46ca1
BECREATOR\aki.zheng <ahsi.aki@gmail.com>
2022-08-18 13:53:57 +0800

    navigate

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import {
   HashRouter,
   NavLink,
   Routes,
-  Route
+  Route,
+  useNavigate
 } from 'react-router-dom';
+import * as antd from 'antd';
+import { useState } from 'react';
+
 
 
 
@@ -14,8 +18,17 @@ import {
 // 讓上方的 Nav 選單可切換到各頁面。
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  const navigate = useNavigate();
+  const [user, setUser] = useState({name:"王小明", id:'ming'}) 
+  const logout = () => { 
+    navigate('/login',{replace:true,state:user})
+  }
+
+  return (
+    <div className="container">
+      <antd.Button onClick={logout}>登出</antd.Button>
+    </div>
+  )
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
@@ -24,7 +37,13 @@ const Register = () => {
   return <p>這是註冊頁面</p>;
 };
 
+//在 Todo 元件中加入 Logout 元件
+//Logout 元件有一顆登出按鈕，點選登出後，使用 navigate 導回 /login 畫面。
+
 function App() {
+
+
+
   return (
     <div className="container">
       <HashRouter>
